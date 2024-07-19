6164ca7952e9c545dbca1abe2420789e727749bc
LightDDark <myavraham25@gmail.com>
2024-07-19 19:16:50 +0300

    Fixed test bugs

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,15 +1,23 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-
-driver = webdriver.Firefox()
+import sys
 
 
 def test_score_service(url):
+    driver = webdriver.Firefox()
     driver.implicitly_wait(5)
     driver.get(url)
     score = driver.find_element(By.ID, 'score').text
+    driver.quit()
     return 0 if score == '4' else -1
 
 
 def main_function():
-    return test_score_service()
+    if len(sys.argv) != 2:
+        print("Usage: python test.py <url>")
+        sys.exit(-1)
+    sys.exit(test_score_service(sys.argv[1]))
+
+
+if __name__ == "__main__":
+    main_function()
