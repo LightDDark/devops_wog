41135886097a586b8fe3b53f82dbfb0a093cf2b0
Or Avraham Yehezkel <77627113+LightDDark@users.noreply.github.com>
2024-07-25 20:48:27 +0300

    Update e2e.py
    Added Chrome options

--- tests/e2e.py
+++ tests/e2e.py
@@ -1,10 +1,14 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import sys
 
 
 def test_score_service(url):
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(5)
     driver.get(url)
     score = int(driver.find_element(By.ID, 'score').text)
