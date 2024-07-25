2d0cb1d510b136fd03ca4b2b5a13d6fe2f7b4603
Or Avraham Yehezkel <77627113+LightDDark@users.noreply.github.com>
2024-07-25 20:56:55 +0300

    Update e2e.py

--- tests/e2e.py
+++ tests/e2e.py
@@ -8,6 +8,7 @@ def test_score_service(url):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(options=chrome_options)
     driver.implicitly_wait(5)
     driver.get(url)
